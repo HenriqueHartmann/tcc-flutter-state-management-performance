032faa48799eb923f74b725377307c9c1684a925
HenriqueHartmann <alemaohartmann@gmail.com>
2025-03-28 00:43:10 -0300

    base_app - add - add list variable to _HomePageState

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_base_gestao_estado/item_card_style.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,5 +9,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+    final List<Map<String, dynamic>> items = [
+    {
+      'title': 'Item 1',
+      'description': 'Descrição do item 1',
+      'style': const ItemCardStyle(),
+    },
+    {
+      'title': 'Item 2',
+      'description': 'Descrição do item 2',
+      'style': const ItemCardStyle(),
+    },
+    {
+      'title': 'Item 3',
+      'description': 'Descrição do item 3',
+      'style': const ItemCardStyle(),
+    },
+  ];
   // ...
 }
