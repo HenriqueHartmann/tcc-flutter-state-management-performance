5595414d13fde1ccf881bbc15d71b5b25e6f0fd5
HenriqueHartmann <alemaohartmann@gmail.com>
2025-03-28 01:08:02 -0300

    base_app - add - add build method to home_page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:app_base_gestao_estado/item_card_style.dart';
 import 'package:flutter/material.dart';
+import 'package:app_base_gestao_estado/item_card_style.dart';
+import 'package:app_base_gestao_estado/item_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage ({ super.key });
@@ -26,5 +27,21 @@ class _HomePageState extends State<HomePage> {
       'style': const ItemCardStyle(),
     },
   ];
-  // ...
+  
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('Lista de Itens')),
+    body: ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ItemCard(
+          title: items[index]['title']!,
+          description: items[index]['description']!,
+          style: items[index]['style'], // Passa o estilo individual
+        );
+      },
+    ),
+  );
+}
 }
