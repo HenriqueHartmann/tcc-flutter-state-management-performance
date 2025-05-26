980ef2e1129330df566f4e5132526c5f41a691ef
HenriqueHartmann <alemaohartmann@gmail.com>
2025-05-26 20:48:23 -0300

    base_app - add item model

---
+++ lib/models/item.dart
@@ -0,0 +1,19 @@
+class Item {
+  final int id;
+  final String title;
+  final String description;
+
+  Item({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] as int,
+      title: map['title'] as String,
+      description: map['description'] as String,
+    );
+  }
+}
